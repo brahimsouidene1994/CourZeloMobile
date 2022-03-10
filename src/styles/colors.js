ee6086389dc2ad63168c62bda8ba3133dce5d6c5
brahimsouidene1994 <barhoumsouidene@gmail.com>
2022-03-10 13:06:50 +0100

    test

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,14 +1,8 @@
-export const PRIMARY = '#1779ba';
-export const SECONDARY = '#767676';
+export const PRIMARY = '#07294d'; //blue
+export const SECONDARY = '#edf0f2'; // gray
 export const WHITE = '#FFFFFF';
 export const BLACK = '#000000';
 
-// ACTIONS
-export const SUCCESS = '#3adb76';
-export const WARNING = '#ffae00';
-export const ALERT = '#cc4b37';
-
-// GRAYSCALE
-export const GRAY_LIGHT = '#e6e6e6';
-export const GRAY_MEDIUM = '#cacaca';
-export const GRAY_DARK = '#8a8a8a';
+// ACTIONS Buttons
+export const FORM_BTN = '#ffc600';
+export const ICON_BTN = '#07294d';
